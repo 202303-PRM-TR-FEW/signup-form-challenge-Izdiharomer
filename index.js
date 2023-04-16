4c24079b4cde7edb3a67157a91e759389b54c075
Izdihar <ezdiharomerrrr@gmail.com>
2023-04-16 14:47:37 +0300

    .

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const submitButton = document.getElementById("submitButton");
-const form = document.getElementById("form");
-const error = document.getElementById("error");
-
-function validateEmail(email) {
-  const regex = /^\S+@\S+\.\S+$/;
-  return regex.test(email);
-}
-
-submitButton.addEventListener("click", function(e) {
-  e.preventDefault();
-  if (firstName.value === '') {
-      alert('You should enter your first name')
-  } else if (lastName.value === '') {
-      alert('You should enter your last name')
-  } else if (email.value === '') {
-      alert('You should enter an email address')
-  } else if (!validateEmail(email.value)) {
-      alert ('Please enter a valid email address')
-  } else if (password.value === ''){
-      alert('You should enter your password')
-  } else if (password.value.length < 8 ) {
-      alert('Your password must be at least 8 characters long')
-  } else {
-      alert('The form is successfully submitted')
+// Create a function to validate input fields
+function validateInput(inputField) {
+    if (inputField.value === '') {
+      inputField.nextElementSibling.style.display = 'block';
+      inputField.style.border = '1px solid #f00';
+      return false;
+    } else {
+      inputField.nextElementSibling.style.display = 'none';
+      inputField.style.border = '1px solid blue';
+      return true;
+    }
   }
-});
+  
+  // Add event listeners to all input fields
+  const firstName = document.getElementById('firstName');
+  const lastName = document.getElementById('lastName');
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  
+  firstName.addEventListener('blur', function() {
+    validateInput(firstName);
+  });
+  
+  lastName.addEventListener('blur', function() {
+    validateInput(lastName);
+  });
+  
+  email.addEventListener('blur', function() {
+    validateInput(email);
+  });
+  
+  password.addEventListener('blur', function() {
+    validateInput(password);
+  });
+  
+  //Add event listener to form submit button
+  const submitButton = document.getElementById('submitButton');
+  const form = document.getElementById('form');
+  const error = document.getElementById('error');
+  
+  form.addEventListener('submit', function(e) {
+    e.preventDefault(); // prevent form submission
+  
+    const isValid = validateInput(firstName) && validateInput(lastName) && validateInput(email) && validateInput(password);
+  
+    if (!isValid) {
+      error.innerHTML = 'Please fill in all fields';
+      return;
+    }
+  
+    // If form is valid, submit it
+    //...
+  });
+  
